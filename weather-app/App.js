28ad611b638d26f2e0b49f0035789354331314f8
salybu <rkdmsdl93@gmail.com>
2022-04-14 11:33:59 +0900

    feat: do some flex things (#2.4 Layout System)

--- weather-app/App.js
+++ weather-app/App.js
@@ -1,24 +1,12 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello This is my first RN App </Text>
-      <StatusBar style="light" />
+    <View style={{ flex: 1 }}>
+      <View style={{ flex: 1, backgroundColor: "tomato" }}></View>
+      <View style={{ flex: 1, backgroundColor: "teal" }}></View>
+      <View style={{ flex: 1, backgroundColor: "orange" }}></View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "blue",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  text: {
-    fontSize: 28,
-    color: "red",
-  },
-});
